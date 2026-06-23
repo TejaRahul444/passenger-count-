9a88145cb8a11065c56f366ddbf52194d5d277b1
TejaRahul444 <maruteja.rahul.cer24@itbhu.ac.in>
2026-06-23 07:20:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+let count = 0;
+function increment() {
+     count=count+1;
+      document.getElementById("count-el").textContent = count;
+}
+function decrement() {
+     count=count-1;
+      document.getElementById("count-el").textContent = count;
+}
